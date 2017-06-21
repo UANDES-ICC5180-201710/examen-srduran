8fbdb12ed43a40b0673927bc83c8cf25004cce4a
srduran <srduran@miuandes.cl>
2017-06-21 08:06:57 -0400

    point 4 and 3 ready

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_member!, :only => [:index], raise: false
 
   # GET /games
   # GET /games.json
