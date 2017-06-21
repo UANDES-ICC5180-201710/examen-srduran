bcdb138a6843c1dc4cab4aac154e9c95b2800619
srduran <srduran@miuandes.cl>
2017-06-21 07:59:21 -0400

    login and logout user

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  validates :title, presence: true, length: {in:10..50}, uniqueness: true
+  validates :description, presence: true, length: {minimum: 30}
+  validates :image_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: {in:3..30}
+  validates :last_name, presence: true, length: {in:3..30}
+  validates :email, presence: true, length: {maximum: 50}, uniqueness: true
+  #validates_format_of :email, :with => Devise::email_regexp
+  validates :password, presence: true, length: {minimum: 8}, confirmation: true
 end
